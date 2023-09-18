b8dadc2b7ff49cc81fe0bd28bca0cc15755717dd
Phyrus Framework <framework@phyrus.org>
2023-09-18 19:31:53 +0200

    Switch webpack to Vite

--- project/vue.config.js
+++ project/vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
+import { defineConfig } from "vite";
+import vue from '@vitejs/plugin-vue';
+const path = require('path');
+
+// https://vitejs.dev/config
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
 })
